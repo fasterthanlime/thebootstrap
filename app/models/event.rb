50a5e3e9c5b1da129e0315a31ebfb8e655e7b320
Amos Wenger <amos@official.fm>
2012-03-14 20:06:12 +0000

    Hide past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,6 +11,6 @@ class Event < ActiveRecord::Base
     validates :creator, :presence => true
 
     def self.upcoming_events
-      Event.where("occurs_at >= ?", Time.now.to_date).order('occurs_at ASC').all(:limit => 10)
+      Event.where("occurs_at >= ?", Time.now).order('occurs_at ASC').all(:limit => 10)
     end
 end
