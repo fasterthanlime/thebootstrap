d33bdc510ec4febcb3a96bdefc1809b39ffcc958
Amos Wenger <amos@official.fm>
2012-03-21 02:43:48 +0000

    Alright, links!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,35 @@ class ApplicationController < ActionController::Base
   before_filter :require_login
 
 private
+  URL_REGEXP = /[^"^'^>]((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/[^ ^<]*)?)/
+  LINK_REGEXP = /<a href=['"](.*)['"]>(.*)<\/a>/
+
   def twitter(nick)
       "<a href='https://twitter.com/#{nick}'><span class='symbol'>t</span>&nbsp;#{nick}</a>"
   end
 
+  def pretty_link(link, text)
+      uri = URI(link).normalize
+      actual_text = if (link == text)
+        "#{uri.host}#{uri.path}"
+      else
+        text
+      end
+      "<a class='url' href='#{link}'><img src='http://getfavicon.appspot.com/#{link}'> #{actual_text}</a>"
+  end
+
   def markup(markdown)
-      html = RDiscount.new(markdown).to_html
+      html = RDiscount.new(' ' + markdown).to_html
       with_twitters = html.gsub(/@(\w*)/, twitter('\1'))
-      with_twitters 
+      with_implicit_links = with_twitters.gsub(URL_REGEXP, ' <a href="\1">\1</a>')
+      with_pretty_links = with_implicit_links.gsub(LINK_REGEXP) do
+          pretty_link($1, $2)
+      end
   end
 
   helper_method :markup
   helper_method :twitter
+  helper_method :pretty_url
 
   def require_login
       redirect_to '/login' if not current_user
